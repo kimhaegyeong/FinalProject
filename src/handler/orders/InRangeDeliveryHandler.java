7f84c69ac0e8c2927c03958a8c07252dae14ca1f
kimhaegyeong <schema9@gmail.com>
2016-07-22 17:18:09 +0900

    일정 범위 안의 배달업체 정보 요청

---
+++ src/handler/orders/InRangeDeliveryHandler.java
@@ -0,0 +1,56 @@
+package handler.orders;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import delivery.DeliveryDao;
+import delivery.DeliveryDataBean;
+import handler.CommandHandler;
+
+@Controller
+public class InRangeDeliveryHandler implements CommandHandler {
+	@Resource(name="deliveryDao")
+	private DeliveryDao deliveryDao;
+		
+	@RequestMapping("/inRangeDelivery")
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		
+		double swLat = Double.parseDouble(request.getParameter("swLat"));
+		double swLng = Double.parseDouble(request.getParameter("swLng"));
+		double neLat = Double.parseDouble(request.getParameter("neLat"));
+		double neLng = Double.parseDouble(request.getParameter("neLnt"));
+		
+		// System.out.println(swLat + " | " + swLng);
+		// System.out.println(neLat + " | " + neLng);
+		
+		Map<String, Double> map = new HashMap<String, Double> (); 
+		map.put("swLat", swLat);
+		map.put("swLng", swLng);
+		map.put("neLat", neLat);
+		map.put("neLng", neLng);
+		
+		List<DeliveryDataBean> deliveryList = deliveryDao.getInRangeMember(map);
+		
+		/*
+		System.out.println("deliveryList.size : " + deliveryList.size() );
+		if (deliveryList != null || deliveryList.size() != 0) {
+			for (DeliveryDataBean item : deliveryList) {
+				System.out.println(item.getEmail());
+			}
+		}
+		*/
+		
+		return new ModelAndView("orders/inRangeDelivery.js");
+	}
+
+}
